63851a67fa516e5265e54053b39cb989d50f1a63
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-09-11 22:47:27 +0300

    feat: remove cache attr

--- makes.nix
+++ makes.nix
@@ -1,16 +1,4 @@
-_: {
-  cache = {
-    readNixos = true;
-    extra = {
-      makes = {
-        enable = true;
-        pubKey = "makes.cachix.org-1:zO7UjWLTRR8Vfzkgsu1PESjmb6ymy1e4OE9YfMmCQR4=";
-        type = "cachix";
-        url = "https://makes.cachix.org";
-        write = true;
-      };
-    };
-  };
+{...}: {
   lintNix = {
     enable = true;
     targets = ["/"];
