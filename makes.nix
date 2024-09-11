a3f83b2e386651c6e6669e53c9f756d0bbe8e5a8
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-09-11 19:54:22 +0300

    feat: add makes

---
+++ makes.nix
@@ -0,0 +1,22 @@
+_: {
+  cache = {
+    readNixos = true;
+    extra = {
+      makes = {
+        enable = true;
+        pubKey = "makes.cachix.org-1:zO7UjWLTRR8Vfzkgsu1PESjmb6ymy1e4OE9YfMmCQR4=";
+        type = "cachix";
+        url = "https://makes.cachix.org";
+        write = true;
+      };
+    };
+  };
+  lintNix = {
+    enable = true;
+    targets = ["/"];
+  };
+  formatNix = {
+    enable = true;
+    targets = ["/nix" "/flake.nix" "/makes.nix"];
+  };
+}
