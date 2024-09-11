592398574a234475bca76ebf0113335b4e40b6de
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-09-11 19:49:36 +0300

    feat(flake): add makes input

--- flake.nix
+++ flake.nix
@@ -1,11 +1,21 @@
 {
   description = "CONFIGURE-ME";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  inputs.std.url = "github:divnix/std";
-  inputs.std.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.std.inputs.devshell.url = "github:numtide/devshell";
-  inputs.std.inputs.nixago.url = "github:nix-community/nixago";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    std = {
+      url = "github:divnix/std";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        devshell.url = "github:numtide/devshell";
+        nixago.url = "github:nix-community/nixago";
+      };
+    };
+    makes = {
+      url = "github:fluidattacks/makes";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   nixConfig = {
     extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
